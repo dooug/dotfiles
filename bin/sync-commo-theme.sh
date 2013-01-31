c5f760f26e74a2c026f3dff4cb1e44f3d2bcfc57
Benjamin Chodoroff <ben@theworkdept.com>
2013-01-31 16:06:29 -0500

    tweakin

--- bin/sync-commo-theme.sh
+++ bin/sync-commo-theme.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-scp -r ~/commo/luci-theme-commotion/files/htdocs/* root@192.168.1.118:/www/luci-static/commotion/
-scp ~/commo/luci-theme-commotion/files/templates/* root@192.168.1.118:/usr/lib/lua/luci/view/themes/commotion/
+scp -r ~/Desktop/codez/commotion-openwrt-theme/files/htdocs/* root@192.168.1.118:/www/luci-static/commotion/
+scp ~/Desktop/codez/commotion-openwrt-theme/files/templates/* root@192.168.1.118:/usr/lib/lua/luci/view/themes/commotion/
+scp ~/Desktop/codez/commotion-openwrt-theme/files/luasrc/view/cbi/* root@192.168.1.118:/usr/lib/lua/luci/view/cbi/
